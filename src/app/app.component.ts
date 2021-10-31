d6eac7dbe12fd6a2845aa7768a869557110fb52d
Emir TARTAR <emirtartar35@gmail.com>
2021-11-01 00:18:42 +0300

    Projeye bootstrap dahil edildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ReCapProjectFrontend';
+  title:string = 'ReCap Project Frontend';
 }
